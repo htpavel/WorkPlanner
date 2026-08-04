fd5325d2e0a076394722036282449329a940536e
Pavel Černý <htpavel@seznam.cz>
2026-08-04 19:51:40 +0200

    feat: refactor server setup to include dotenv configuration and improve logging

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const userRoutes = require('./routes/user-routes');
 const routeRoutes = require('./routes/route-routes');
-const addressRoutes = require('./routes/address-routes');
 
 const app = express();
-app.use(express.json());
-const PORT = 3000;
 
-// Použití samostatných rout s prefixy
+app.use(cors());
+app.use(express.json());
+
+// Zapojení routerů jako middleware
 app.use('/api/user', userRoutes);
 app.use('/api/route', routeRoutes);
-app.use('/api/address', addressRoutes);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Dokonale modulární backend běžící na http://localhost:${PORT}`);
+    console.log(`Server běží na portu ${PORT}`);
 });
